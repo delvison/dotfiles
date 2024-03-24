acd4544b1fbc8b04ebe566cd247cf1694ab01506
delvison <delvison.castillo@protonmail.me>
2024-03-23 20:16:21 -0400

    add scripts for note-taking

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -24,6 +24,7 @@ alias ncmpcpp='ncmpcpp -b .config/ncmpcpp/bindings'
 alias "git log"="git log --show-signature"
 alias open="xdg-open"
 alias lzd='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.config/lazydocker:/.config/jesseduffield/lazydocker lazyteam/lazydocker'
+alias sb="cd ~/Nextcloud/40_Documents/42_Notes/42.01_Obsidian/"
 
 # Enable Ctrl-x-e to edit command line
 autoload -U edit-command-line
