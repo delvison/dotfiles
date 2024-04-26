842f7aa44980750b3a493da6ff7fbfe1c4dc99f7
delvison <delvison.castillo@protonmail.me>
2024-04-26 19:40:59 -0400

    nix: 35 current  2024-04-25 08:22:03  23.11.20240322.56528ee  6.1.82

--- .config/nixos/scripts/build.sh
+++ .config/nixos/scripts/build.sh
@@ -2,9 +2,9 @@
 set -e
 
 pushd ~/.config/nixos
-git --git-dir=/home/npc/.dotfiles --work-tree=/home/npc diff .
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME diff .
 sudo nixos-rebuild switch --impure --flake ~/.config/nixos#$HOST
 gen=$(nixos-rebuild list-generations | grep current | tr -d '*')
-git --git-dir=/home/npc/.dotfiles --work-tree=/home/npc add .
-git --git-dir=/home/npc/.dotfiles --work-tree=/home/npc commit -S -am "nix: $gen"
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME commit -S -am "nix: $gen"
 popd
