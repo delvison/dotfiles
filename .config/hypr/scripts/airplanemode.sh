f66cbd143aa82a0e14cf6ec7275b46e0878cd061
delvison <delvison.castillo@protonmail.me>
2024-05-15 22:12:10 -0400

    hyprland: add airplane mode script

---
+++ .config/hypr/scripts/airplanemode.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+aplay ~/.config/hypr/assets/pop2.wav
+if [ -f "/tmp/.airplanemode" ]; then
+  nmcli r all on
+  rm /tmp/.airplanemode
+  notify-send -t 1000 "Airplane mode" "off" -u low -r 9000
+else
+  touch /tmp/.airplanemode
+  nmcli r all off
+  notify-send -t 1000 "Airplane mode" "on" -u low -r 9000
+fi
