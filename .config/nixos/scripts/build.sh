fcef1e3b9844687234fd89120878e6a057de895d
delvison <delvison.castillo@protonmail.me>
2024-05-17 07:51:02 -0400

    nix: 70 current  2024-05-17 07:46:10  23.11.20240322.56528ee  6.1.82

--- .config/nixos/scripts/build.sh
+++ .config/nixos/scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 pushd ~/.config/nixos
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME diff .
 sudo nixos-rebuild switch --impure --flake ~/.config/nixos#$HOST
-print -P -n "%F{green}Nix config successfully built.%F{reset}\n"
-echo "committing diff..."
+print -P -n "%F{green}✅ Nix config successfully built.%F{reset}\n"
+echo "💽 committing diff..."
 gen=$(nixos-rebuild list-generations | grep current | tr -d '*')
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME commit -S -am "nix: $gen"
