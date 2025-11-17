f960471de8a42bfd861399df9e18daf3507dc458
delvison <delvison.castillo@protonmail.me>
2025-11-17 07:49:42 -0500

    fix display scripts

--- .config/xrandr/default.sh
+++ .config/xrandr/default.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+notify-send -t 2000 "Display reverted back to default." -u low
 xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off --output DisplayPort-10 --off
 nitrogen --restore
